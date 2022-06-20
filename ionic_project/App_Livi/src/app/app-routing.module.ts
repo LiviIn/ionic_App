97292e7b391e604a27860a308632bee4b72225d9
Livin R <liviin651@gmail.com>
2022-06-20 17:06:05 +0530

    add home page design

--- ionic_project/App_Livi/src/app/app-routing.module.ts
+++ ionic_project/App_Livi/src/app/app-routing.module.ts
@@ -2,9 +2,18 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // }
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    redirectTo: 'tabs',
+    pathMatch: 'full'
+  },
+  {
+    path: 'tabs',
+    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 @NgModule({
