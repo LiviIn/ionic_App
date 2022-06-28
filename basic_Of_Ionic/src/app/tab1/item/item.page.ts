a2e30f3134e70b1cae82f3568fd5be73deab3bdb
Livin R <liviin651@gmail.com>
2022-06-28 17:27:50 +0530

    spreadOperators

--- basic_Of_Ionic/src/app/tab1/item/item.page.ts
+++ basic_Of_Ionic/src/app/tab1/item/item.page.ts
@@ -27,6 +27,20 @@ export class ItemPage implements OnInit {
       if(val){
         this.items = data;
       }
+      // only for overwriting is apply in object array values are not overwrites only add values in array
+
+      const object1 = {...this.items[0], price: 12, p_code: 10};// is used to add and overwriting the values
+      // const object2 = {...object1}; // ... is a Spread Operators
+      console.log('spreadOperators :', object1 )
+
+      // const array1 = [...this.items, {id: 4, name: 'p3'}]; // is only for add not overwrite
+
+      const array1 = [1,2,3,4];
+      const array2 = [1,3,7,8];
+      const array3 = [...array1, ...array2, 0,9];
+      console.log('array3: ', array3)
+
+
     }catch(err){
       console.error(err)
     }
