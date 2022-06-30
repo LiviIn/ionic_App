689cd4b1f261a2a8fdfb148adeff5b4eccde3444
Livin R <liviin651@gmail.com>
2022-06-30 07:48:47 +0530

    add a searchbar in search page

--- basic_Of_Ionic/src/app/pages/tabs/search/search.page.ts
+++ basic_Of_Ionic/src/app/pages/tabs/search/search.page.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,10 +6,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search.page.scss'],
 })
 export class SearchPage implements OnInit {
-
+  @ViewChild('searchInput') sInput:any;
   constructor() { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.sInput.setFocus();
+    }, 500);
+  }
+
+  onSearchChange(event){
+    console.log(event.detail.value)
   }
 
 }
